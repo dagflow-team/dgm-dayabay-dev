ba4a79637b7d505b402ec12ec63b7bf0157293d6
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2023-05-16 10:09:43 +0300

    feat: add mean energy per fission

--- models/dayabay_v0.py
+++ models/dayabay_v0.py
@@ -39,6 +39,7 @@ def model_dayabay_v0():
         storage ^= load_parameters({'path': 'detector'   , 'load': datasource/'parameters/detector_nprotons_correction.yaml'})
         storage ^= load_parameters({'path': 'detector'   , 'load': datasource/'parameters/detector_eres.yaml'})
 
+        storage ^= load_parameters({'path': 'reactor'    , 'load': datasource/'parameters/reactor_e_per_fission.yaml'})
         storage ^= load_parameters({'path': 'reactor'    , 'load': datasource/'parameters/reactor_thermal_power_nominal.yaml'     , 'replicate': list_reactors })
         storage ^= load_parameters({'path': 'reactor'    , 'load': datasource/'parameters/reactor_snf.yaml'                       , 'replicate': list_reactors })
         storage ^= load_parameters({'path': 'reactor'    , 'load': datasource/'parameters/reactor_offequilibrium_correction.yaml' , 'replicate': list_reactors_isotopes })
