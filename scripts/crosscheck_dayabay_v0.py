c814b04edbc721c401912001393acc579cd118ee
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2024-03-19 14:54:42 +0300

    feat: cross check, make a class, add cross section comparison
    start oscprob comparison

--- scripts/crosscheck_dayabay_v0.py
+++ scripts/crosscheck_dayabay_v0.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
 from argparse import Namespace
-from itertools import islice
-from numpy import allclose, fabs
+from itertools import islice, permutations
+from typing import Any
+
+from h5py import Dataset, File, Group
+from numpy import allclose, fabs, nanmax
 from numpy.typing import NDArray
 
-from h5py import File
-
-from dagflow.logger import INFO1, INFO2, INFO3, set_level, logger
+from dagflow.logger import INFO1, INFO2, INFO3, logger, set_level
+from dagflow.output import Output
 from models.dayabay_v0 import model_dayabay_v0
+from multikeydict.nestedmkdict import NestedMKDict
 
 set_level(INFO1)
 
@@ -18,78 +21,146 @@ comparison_objects = {
     "edges.energy_edep": "evis_edges",
     "kinematics_sampler.mesh_edep": "evis_mesh",
     "ibd.enu": {
-        "name": "enu",
+        "gnaname": "enu",
         "slice": slice(102, None),
         "ignore": "Different formula below 2me. Not used as it is below the threshold.",
         "atol": 1e-14,
     },
     "ibd.jacobian": {
-        "name": "jacobian",
+        "gnaname": "jacobian",
         "atol": 1e-15,
     },
+    "ibd.crosssection": {
+        "gnaname": "ibd_xsec",
+        "rtol": 1.e-14
+        },
+    "oscprob": {
+        "gnaname": "osc_prob_rd"
+        }
 }
 # fmt: on
 
 
-def main(opts: Namespace) -> None:
-    if opts.verbose:
-        opts.verbose = min(opts.verbose, 3)
-        set_level(globals()[f"INFO{opts.verbose}"])
+class Comparator:
+    opts: Namespace
+    output_dgf: NestedMKDict
 
-    model = model_dayabay_v0(source_type=opts.source_type)
-    outputs_dgf = model.storage("outputs")
+    _cmpopts: dict[str, Any]
+    _maxdiff: float = 0.0
+    _maxreldiff: float = 0.0
 
-    if opts.last:
-        iterable = islice(reversed(comparison_objects.items()), 1)
-    else:
-        iterable = comparison_objects.items()
-    for key_dgf, key_gna in iterable:
-        if isinstance(key_gna, dict):
-            key_gna, cmpopts = key_gna["name"], key_gna
+    _skey_gna: str
+    _skey_dgf: str
+
+    def __init__(self, opts: Namespace):
+        self.model = model_dayabay_v0(source_type=opts.source_type)
+        self.opts = opts
+
+        if opts.verbose:
+            opts.verbose = min(opts.verbose, 3)
+            set_level(globals()[f"INFO{opts.verbose}"])
+
+        self.outputs_dgf = self.model.storage("outputs")
+
+        self.compare()
+
+    def compare(self) -> None:
+        if self.opts.last:
+            iterable = islice(reversed(comparison_objects.items()), 1)
         else:
-            cmpopts = {}
+            iterable = comparison_objects.items()
+        for self._skey_dgf, cmpopts in iterable:
+            match cmpopts:
+                case dict():
+                    self._skey_gna= cmpopts["gnaname"]
+                    self._cmpopts = cmpopts
+                case str():
+                    self._skey_gna = cmpopts
+                    self._cmpopts = {}
+                case _:
+                    raise RuntimeError(f"Invalid {cmpopts=}")
 
-        path_gna = key_gna.replace(".", "/")
-        data_gna = opts.input[path_gna][:]
+            self.compare_source()
 
-        data_dgf = outputs_dgf[key_dgf].data
+    def compare_source(self) -> None:
+        path_gna = self._skey_gna.replace(".", "/")
 
-        is_ok, maxdiff = data_consistent(data_gna, data_dgf, **cmpopts)
+        data_storage_gna = self.opts.input[path_gna]
+        data_storage_dgf = self.outputs_dgf.any(self._skey_dgf)
+
+        match data_storage_dgf, data_storage_gna:
+            case Output(), Dataset():
+                data_gna = data_storage_gna[:]
+                data_dgf = data_storage_dgf.data
+                self.compare_outputs(data_gna, data_dgf)
+            case NestedMKDict(), Group():
+                self.compare_nested(data_storage_gna, data_storage_dgf)
+            case _:
+                raise RuntimeError("Unexpected data types")
+
+    def compare_outputs(self, data_gna: NDArray, data_dgf: NDArray):
+        is_ok = self.data_consistent(data_gna, data_dgf)
         if is_ok:
-            logger.log(INFO1, f"OK: {key_dgf}↔{key_gna}")
-            if (ignore:=cmpopts.get("ignore")) is not None:
+            logger.log(INFO1, f"OK: {self._skey_dgf}↔{self._skey_gna}")
+            if (ignore := self._cmpopts.get("ignore")) is not None:
                 logger.log(INFO2, f"↑Ignore: {ignore}")
         else:
-            logger.error(f"FAIL: {key_dgf}↔{key_gna}, max diff {maxdiff:.2g}")
+            logger.error(
+                f"FAIL: {self._skey_dgf}↔{self._skey_gna}, "
+                f"max diff {self._maxdiff:.2g}, "
+                f"max rel diff {self._maxreldiff:.2g}"
+            )
 
-            if opts.embed_on_fail:
-                import IPython; IPython.embed(colors='neutral')
+            if self.opts.embed_on_fail:
+                diff = data_dgf - data_gna
+                import IPython
 
-def data_consistent(
-    gna: NDArray,
-    dgf: NDArray,
-    *,
-    slice: tuple[slice | None,...] | None = None,
-    slice_gna: tuple[slice | None,...] | None = None,
-    atol = 0,
-    rtol = 0,
-    # not used:
-    name: str = "",
-    ignore: str | None = None
-) -> tuple[bool, float]:
-    if slice_gna is not None:
-        gna = gna[slice_gna]
-    elif slice is not None:
-        gna = gna[slice]
-        dgf = dgf[slice]
-    status = allclose(dgf, gna, rtol=rtol, atol=atol)
+                IPython.embed(colors="neutral")
 
-    maxdiff = fabs(dgf-gna).max()
+    def compare_nested(self, storage_gna: Group, storage_dgf: NestedMKDict):
+        for key_d, output_dgf in storage_dgf.walkitems():
+            data_d = output_dgf.data
+            for key_g in permutations(key_d):
+                path_g = '/'.join(key_g)
 
-    if status:
-        return True, maxdiff
+                try:
+                    data_g = storage_gna[path_g]
+                except KeyError:
+                    continue
 
-    return False, maxdiff
+                self.compare_outputs(data_g[:], data_d)
+
+    def data_consistent(
+        self,
+        gna: NDArray,
+        dgf: NDArray,
+        *,
+        slice: tuple[slice | None, ...] | None = None,
+        slice_gna: tuple[slice | None, ...] | None = None,
+        atol=0,
+        rtol=0,
+        # not used:
+        gnaname: str = "",
+        ignore: str | None = None,
+    ) -> bool:
+        if (slice_gna:=self._cmpopts.get("slice_gna")) is not None:
+            gna = gna[slice_gna]
+        elif (slice:=self._cmpopts.get("slice")) is not None:
+            gna = gna[slice]
+            dgf = dgf[slice]
+        atol = float(self._cmpopts.get("atol", 0.0))
+        rtol = float(self._cmpopts.get("rtol", 0.0))
+        status = allclose(dgf, gna, rtol=rtol, atol=atol)
+
+        fdiff = fabs(dgf - gna)
+        self._maxdiff = float(fdiff.max())
+        self._maxreldiff = float(nanmax(fdiff / gna))
+
+        if status:
+            return True
+
+        return False
+
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -100,9 +171,7 @@ if __name__ == "__main__":
     )
 
     input = parser.add_argument_group("input", "input related options")
-    input.add_argument(
-        "input", type=File, help="input file to compare to"
-    )
+    input.add_argument("input", type=File, help="input file to compare to")
 
     parser.add_argument(
         "-s",
@@ -114,7 +183,11 @@ if __name__ == "__main__":
     )
 
     crosscheck = parser.add_argument_group("comparison", "comparison related options")
-    crosscheck.add_argument("-l", "--last", action="store_true", help="process only the last item")
-    crosscheck.add_argument("-e", "--embed-on-fail", action="store_true", help="embed on failure")
+    crosscheck.add_argument(
+        "-l", "--last", action="store_true", help="process only the last item"
+    )
+    crosscheck.add_argument(
+        "-e", "--embed-on-fail", action="store_true", help="embed on failure"
+    )
 
-    main(parser.parse_args())
+    c = Comparator(parser.parse_args())
