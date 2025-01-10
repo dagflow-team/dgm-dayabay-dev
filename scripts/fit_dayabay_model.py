0190bec1e0fade27aa632c1cd4f7aa509b332ae5
Zavadskyi Vitalii <zavadskiy.vs@phystech.edu>
2025-01-10 18:43:13 +0300

    feature, WIP: update fit plotting script

--- scripts/fit_dayabay_model.py
+++ scripts/fit_dayabay_model.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python
 from argparse import Namespace
 
-import yaml
+from yaml import dump as yaml_dump
+import numpy as np
 from matplotlib import pyplot as plt
 
 from dagflow.tools.logger import DEBUG as INFO4
 from dagflow.tools.logger import INFO1, INFO2, INFO3, set_level
-from dagflow.parameters.gaussian_parameter import GaussianParameter
+from dagflow.core import NodeStorage
+from dagflow.parameters import Parameter, GaussianParameter
 from dagflow.tools.yaml_dumper import convert_numpy_to_lists, filter_fit
 from dgf_statistics.minimizer.iminuitminimizer import IMinuitMinimizer
-from models import available_models, load_model
+from models import available_models, load_model, LATEX_SYMBOLS
 
 set_level(INFO1)
 
 DATA_INDICES = {"asimov": 0, "data-a": 1}
 
 
+def update_minimization_paramers(
+    minimization_parameters: dict[str, Parameter], groups: list[str], model_parameters: NodeStorage,
+) -> None:
+    for group in groups:
+        minimization_parameters.update(dict(model_parameters(group).walkjoineditems()))
+
+
 def main(args: Namespace) -> None:
+
+    if args.verbose:
+        args.verbose = min(args.verbose, 3)
+        set_level(globals()[f"INFO{args.verbose}"])
 
     model = load_model(
         args.version,
@@ -26,11 +39,11 @@ def main(args: Namespace) -> None:
         seed=args.seed,
     )
 
-    model.storage["nodes.data.proxy"].switch_input(DATA_INDICES[args.data])
-
-    parameters_free = model.storage("parameters.free")
-    parameters_constrained = model.storage("parameters.constrained")
-    statistic = model.storage("outputs.statistic")
+    storage: NodeStorage = model.storage
+    storage["nodes.data.proxy"].switch_input(DATA_INDICES[args.data])
+    parameters_free = storage("parameters.free")
+    parameters_constrained = storage("parameters.constrained")
+    statistic = storage("outputs.statistic")
 
     parameters_groups = {
         "free": ["oscprob"],
@@ -43,23 +56,20 @@ def main(args: Namespace) -> None:
     if args.use_hm_unc_pull_terms:
         parameters_groups["constrained"].append("reactor_anue")
 
-    chi2 = statistic[f"{args.chi2}"]
-    minimization_parameters = {
-        par_name: par
-        for key in parameters_groups["free"]
-        for par_name, par in parameters_free(key).walkjoineditems()
-    }
+    stat_chi2 = statistic[f"{args.chi2}"]
+    minimization_parameters = {}
+    update_minimization_paramers(
+        minimization_parameters, parameters_groups["free"], parameters_free
+    )
     if "covmat" not in args.chi2:
-        minimization_parameters.update(
-            {
-                par_name: par
-                for key in parameters_groups["constrained"]
-                for par_name, par in parameters_constrained(key).walkjoineditems()
-            }
+        update_minimization_paramers(
+            minimization_parameters,
+            parameters_groups["constrained"],
+            parameters_constrained,
         )
 
     model.next_sample()
-    minimizer = IMinuitMinimizer(chi2, parameters=minimization_parameters)
+    minimizer = IMinuitMinimizer(stat_chi2, parameters=minimization_parameters)
     fit = minimizer.fit()
     print(fit)
 
@@ -67,35 +77,52 @@ def main(args: Namespace) -> None:
         values = []
         errors = []
         labels = []
-        for parname, par in minimization_parameters.items():
+        for i, (parname, par) in enumerate(minimization_parameters.items()):
             if isinstance(par, GaussianParameter):
                 values.append((fit["xdict"][parname] - par.central) / par.sigma)
                 errors.append(abs(fit["errorsdict"][parname] / par.sigma))
-                labels.append(parname)
+                labels.append(LATEX_SYMBOLS[parname])
         npars = len(values)
-        plt.figure(figsize=(5, 0.225 * npars))
-        f_key = labels[0]
-        l_key = labels[-1]
-        plt.scatter(values, labels)
-        # plt.errorbar(values, labels, xerr=errors, linestyle="None")
-        plt.vlines(0, f_key, l_key, linestyle="--", color="black")
-        for i in range(1, 4):
-            plt.vlines(i, f_key, l_key, linestyle="--", color=f"C{i}")
-            plt.vlines(-i, f_key, l_key, linestyle="--", color=f"C{i}")
-        plt.ylim(-1, npars + 1)
+        if npars:
+            fig, axs = plt.subplots(
+                2, 1, figsize=(5, 0.225 * npars),
+                gridspec_kw={"height_ratios": [1, npars / 5]},
+            )
+            for i in range(0, 4):
+                axs[1].vlines([-i, i], -1, npars + 1, linestyle="--", color=f"black", alpha=0.25)
+            axs[0].hist(values, np.linspace(-3.25, 3.25, 2 * int(npars**0.5)))
+            axs[1].scatter(values, labels)
+            for ax in axs:
+                ax.set_xlim(-3.25, 3.25)
+            axs[1].set_ylim(-0.25, npars - 0.75)
+            plt.tight_layout()
+            plt.subplots_adjust(hspace=0)
+            plt.savefig(args.output_plot_pars)
+
+    if args.output_plot_corrmat and fit["covariance"] is not None:
+        covariance = fit["covariance"]
+        npars = len(covariance)
+        diag = np.diagonal(covariance) ** 0.5
+        correlation = covariance / np.outer(diag, diag)
+        if npars < 20:
+            fig, ax = plt.subplots(1, 1)
+        else:
+            fig, ax = plt.subplots(1, 1, figsize=(0.225 * npars, 0.185 * npars))
+        cs = ax.matshow(correlation, vmin=-1, vmax=1, cmap="RdBu")
+        clb = plt.colorbar(cs, ax=ax)
+        clb.set_ticks([-1, -0.5, -0.25, 0, 0.25, 0.5, 1])
+        clb.set_ticklabels([-1, -0.5, -0.25, 0, 0.25, 0.5, 1], fontsize=npars / 4)
+        tick_labels = [LATEX_SYMBOLS[parname] for parname in fit["names"]]
+        ax.set_xticks(np.arange(npars), tick_labels, fontsize=8, rotation=90)
+        ax.set_yticks(np.arange(npars), tick_labels, fontsize=8, rotation=0)
         plt.tight_layout()
-        plt.savefig(args.output_plot_pars)
+        plt.savefig(args.output_plot_corrmat)
 
-    if args.output_plot_covmat:
-        cs = plt.matshow(fit["covariance"])
-        plt.colorbar(cs)
-        plt.savefig(args.output_plot_covmat)
-
-    filter_fit(fit)
+    filter_fit(fit, ["summary"])
     convert_numpy_to_lists(fit)
     if args.output_fit:
         with open(args.output_fit, "w") as f:
-            yaml.dump(fit, f)
+            yaml_dump(fit, f)
 
 
 if __name__ == "__main__":
@@ -157,8 +184,9 @@ if __name__ == "__main__":
         default="stat.chi2p",
         choices=[
             "stat.chi2p_iterative", "stat.chi2n", "stat.chi2p", "stat.chi2cnp",
-            "stat.chi2p_unbiased", "full.chi2p_covmat_fixed", "full.chi2n_covmat",
-            "full.chi2p_covmat_variable", "full.chi2p_iterative", "full.chi2cnp",
+            "stat.chi2p_unbiased", "full.chi2p_covmat_fixed",
+            "full.chi2n_covmat", "full.chi2p_covmat_variable",
+            "full.chi2p_iterative", "full.chi2cnp",
             "full.chi2p_unbiased", "full.chi2cnp_covmat",
         ],
         help="Choose chi-squared function for minimizer",
@@ -184,8 +212,8 @@ if __name__ == "__main__":
         help="path to save plot of normalized values",
     )
     outputs.add_argument(
-        "--output-plot-covmat",
-        help="path to save plot of normalized values",
+        "--output-plot-corrmat",
+        help="path to save plot of correlation matrix of fitted parameters",
     )
 
     args = parser.parse_args()
