6c7030b01307b204de844940a1d0d119eb11f82f
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2025-04-10 17:05:36 +0300

    script: matplotlib params

--- scripts/run_dayabay_v0.py
+++ scripts/run_dayabay_v0.py
@@ -7,6 +7,8 @@ from contextlib import suppress
 from pathlib import Path
 from typing import TYPE_CHECKING
 
+from matplotlib import pyplot as plt
+
 from dagflow.core import Graph, NodeStorage
 from dagflow.tools.logger import DEBUG as INFO4
 from dagflow.tools.logger import INFO1, INFO2, INFO3, set_level
@@ -78,6 +80,7 @@ def main(opts: Namespace) -> None:
         model.index["reactor"],
         model.index["bkg"],
         model.index["detector"],
+        model.index["lsnl"],
     ]
     if opts.plot_all:
         storage("outputs").plot(
@@ -233,59 +236,71 @@ def plot_graph(graph: Graph, storage: NodeStorage, opts: Namespace) -> None:
         maxdepth=0,
     ).savegraph(f"output/dayabay_{opts.version}_stat.dot")
 
+
+plt.rcParams.update(
+    {
+        "axes.grid": False,
+        "xtick.minor.visible": True,
+        "xtick.top": True,
+        "ytick.minor.visible": True,
+        "ytick.right": True,
+        "axes.formatter.limits": (-3, 4),
+    }
+)
+
 latex_substitutions = {
-        " U235": r' $^{235}$U',
-        " U238": r' $^{238}$U',
-        " Pu239": r' $^{239}$Pu',
-        " Pu241": r' $^{241}$Pu',
-        "U235 ": r'$^{235}$U ',
-        "U238 ": r'$^{238}$U ',
-        "Pu239 ": r'$^{239}$Pu ',
-        "Pu241 ": r'$^{241}$Pu ',
-        "Eν": r"$E_{\nu}$",
-        "Edep": r"$E_{\rm dep}$",
-        "Evis": r"$E_{\rm vis}$",
-        "Escint": r"$E_{\rm scint}$",
-        "Erec": r"$E_{\rm rec}$",
-        "cosθ": r"$\cos\theta$",
-        "Δm²₃₁": r"$\Delta m²_{31}$",
-        "Δm²₃₂": r"$\Delta m²_{32}$",
-        "Δm²₂₁": r"$\Delta m²_{21}$",
-        "sin²2θ₁₃": r"$\sin^22\theta_{13}$",
-        "sin²2θ₁₂": r"$\sin^22\theta_{12}$",
-        "sin²θ₁₃": r"$\sin^2\theta_{13}$",
-        "sin²θ₁₂": r"$\sin^2\theta_{12}$",
-        "sin²2θ₁₂": r"$\sin^22\theta_{12}$",
-        "¹³C(α,n)¹⁶O": r"$^{13}{\rm C}(α,n)^{16}{\rm O}$",
-        "²⁴¹Am¹³C": r"$^{241}{\rm Am}^{13}{\rm C}$",
-        "⁹Li/⁸He": r"$^{9}{\rm Li}/^{8}{\rm He}$",
-        "ν̅": r"$\overline{\nu}$",
-        "ν": r"$\nu$",
-        "δ": r"$\delta$",
-        "γ": r"$\gamma$",
-        "μ": r"$\mu$",
-        "σ": r"$\sigma$",
-        "π": r"$\pi$",
-        "χ²": r"$χ^2$",
-        "·": r"$\cdot$",
-        "×": r"$\times$",
-        "⁻¹": r"$^{-1}$",
-        "⁻²": r"$^{-2}$",
-        "²": r"$^2$",
-        "ᵢ": r"$_i$",
-        }
+    " U235": r" $^{235}$U",
+    " U238": r" $^{238}$U",
+    " Pu239": r" $^{239}$Pu",
+    " Pu241": r" $^{241}$Pu",
+    "U235 ": r"$^{235}$U ",
+    "U238 ": r"$^{238}$U ",
+    "Pu239 ": r"$^{239}$Pu ",
+    "Pu241 ": r"$^{241}$Pu ",
+    "Eν": r"$E_{\nu}$",
+    "Edep": r"$E_{\rm dep}$",
+    "Evis": r"$E_{\rm vis}$",
+    "Escint": r"$E_{\rm scint}$",
+    "Erec": r"$E_{\rm rec}$",
+    "cosθ": r"$\cos\theta$",
+    "Δm²₃₁": r"$\Delta m²_{31}$",
+    "Δm²₃₂": r"$\Delta m²_{32}$",
+    "Δm²₂₁": r"$\Delta m²_{21}$",
+    "sin²2θ₁₃": r"$\sin^22\theta_{13}$",
+    "sin²2θ₁₂": r"$\sin^22\theta_{12}$",
+    "sin²θ₁₃": r"$\sin^2\theta_{13}$",
+    "sin²θ₁₂": r"$\sin^2\theta_{12}$",
+    "sin²2θ₁₂": r"$\sin^22\theta_{12}$",
+    "¹³C(α,n)¹⁶O": r"$^{13}{\rm C}(α,n)^{16}{\rm O}$",
+    "²⁴¹Am¹³C": r"$^{241}{\rm Am}^{13}{\rm C}$",
+    "⁹Li/⁸He": r"$^{9}{\rm Li}/^{8}{\rm He}$",
+    "ν̅": r"$\overline{\nu}$",
+    "ν": r"$\nu$",
+    "δ": r"$\delta$",
+    "γ": r"$\gamma$",
+    "μ": r"$\mu$",
+    "σ": r"$\sigma$",
+    "π": r"$\pi$",
+    "χ²": r"$χ^2$",
+    "·": r"$\cdot$",
+    "×": r"$\times$",
+    "⁻¹": r"$^{-1}$",
+    "⁻²": r"$^{-2}$",
+    "²": r"$^2$",
+    "ᵢ": r"$_i$",
+}
 
 exact_substitutions = {
-        "U235": r'$^{235}$U',
-        "U238": r'$^{238}$U',
-        "Pu239": r'$^{239}$Pu',
-        "Pu241": r'$^{241}$Pu',
-        "acc": r"accidentals",
-        "lihe": r"$^{9}{\rm Li}/^{8}{\rm He}$",
-        "fastn": r"fast neutrons",
-        "alphan": r"$^{13}{\rm C}(α,n)^{16}{\rm O}$",
-        "amc": r"$^{241}{\rm Am}^{13}{\rm C}$",
-        }
+    "U235": r"$^{235}$U",
+    "U238": r"$^{238}$U",
+    "Pu239": r"$^{239}$Pu",
+    "Pu241": r"$^{241}$Pu",
+    "acc": r"accidentals",
+    "lihe": r"$^{9}{\rm Li}/^{8}{\rm He}$",
+    "fastn": r"fast neutrons",
+    "alphan": r"$^{13}{\rm C}(α,n)^{16}{\rm O}$",
+    "amc": r"$^{241}{\rm Am}^{13}{\rm C}$",
+}
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
