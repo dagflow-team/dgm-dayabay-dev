ead8d2784cb3c455db0949668604e184adb1f9f4
Vitalii Zavadskyi <zavadskiy.vs@phystech.edu>
2025-10-22 17:48:48 +0300

    feature: add validate load array

---
+++ src/dgm_dayabay_dev/tools/validate_load_array.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from numpy import ndarray, ascontiguousarray
+from collections.abc import Sequence
+from numpy.typing import NDArray
+
+
+def validate_load_array(array: str | Path | Sequence[int | float] | NDArray | None) -> Path | NDArray | None:
+    result = None
+    match array:
+        case str() | Path():
+            result = Path(array)
+            if not result.is_file():
+                raise FileNotFoundError(f"File {result} is not found")
+        case Sequence() | ndarray():
+            result = ascontiguousarray(array, dtype="d")
+        case None:
+            result = None
+        case _:
+            raise RuntimeError(
+                f"Invalid {f'{array=}'.split('=')[0]} type: {type(array).__name__}"
+            )
+    return result
