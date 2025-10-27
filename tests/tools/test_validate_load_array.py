398dcc17b14aa00ada5cdeffbd6b3d122095e9a8
Vitalii Zavadskyi <zavadskiy.vs@phystech.edu>
2025-10-27 17:10:00 +0300

    feature: update tests

--- tests/tools/test_validate_load_array.py
+++ tests/tools/test_validate_load_array.py
@@ -1,6 +1,6 @@
-import numpy as np
 from pathlib import Path
 
+import numpy as np
 from pytest import raises
 
 from dgm_dayabay_dev.tools.validate_load_array import validate_load_array
@@ -9,8 +9,8 @@ from dgm_dayabay_dev.tools.validate_load_array import validate_load_array
 def test_validate_load_array():
     array_path = Path("data/dayabay-v1a/parameters-common/reactor_antineutrino_spectrum_edges.tsv")
 
-    assert validate_load_array(array_path) == array_path 
-    assert validate_load_array(str(array_path)) == array_path 
+    assert validate_load_array(array_path) == array_path
+    assert validate_load_array(str(array_path)) == array_path
 
     edges_np = np.linspace(0, 12, 13)
     assert np.allclose(validate_load_array(edges_np), edges_np)
